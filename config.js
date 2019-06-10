3bc4f4e2d623804a74b2e6d8264e0f6dbe4bab8d
tareef mando <tareef@fastmail.com>
2019-06-10 19:16:31 +0300

    init commit

---
+++ config.js
@@ -0,0 +1,7 @@
+export const siteName = 'Sudanses Archive';
+export const siteUrl = 'https://sudansesarchive.org/';
+export const siteDescription = ' Preserving Documentation of Human rights Violations';
+export const generatorName = 'Metalsmith';
+export const generatorUrl = 'http://metalsmith.io/';
+export const contentSource = '../sudansesarchive-investigations';
+
